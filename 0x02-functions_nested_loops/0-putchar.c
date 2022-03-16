4007a5e4b0ce67500b4e3f532f2208c511ea9cea
dekenzo <79919801+Kebirna@users.noreply.github.com>
2022-03-16 18:30:46 +0800

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,13 @@
 #include "main.h"
-
 /**
  * main - Print "-putchar"
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i = 0;
 	char name[] = "_putchar";
-
 	while (i < 9)
 	{
 		_putchar(name[i]);
