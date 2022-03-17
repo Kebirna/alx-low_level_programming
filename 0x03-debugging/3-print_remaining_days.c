dbbf944504d3dc84165053df227bbea7b8271b70
dekenzo <79919801+Kebirna@users.noreply.github.com>
2022-03-17 17:43:34 +0800

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
 * print_remaining_days - takes a date and prints how many days are
@@ -12,18 +12,30 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	int l;
+
+	if (year % 400 == 0)
+		l = 1;
+	else if (year % 100 == 0)
+		l = 0;
+	else if (year % 4 == 0)
+		l = 1;
+	else
+		l = 0;
+
+	if (l == 1)
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
+
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-		if (month == 2 && day >= 60)
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
