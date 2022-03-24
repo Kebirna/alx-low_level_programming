ce6a2621fbab07b57a9718db48e0d8e1043edd38
dekenzo <79919801+Kebirna@users.noreply.github.com>
2022-03-24 16:42:31 +0800

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,29 +1,30 @@
 #include "main.h"
 
+#define ROT13IN  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+#define ROT13OUT "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+
 /**
- * print_number - prints an integer.
- * @n: integer
+ * print_number - using only _putchar
+ * @n: the integer to print
+ *
  * Return: void
  */
-
 void print_number(int n)
 {
-int divisor = 1, i, resp;
+	int i, p;
 
-if (n < 0)
-{
-	_putchar('-');
-	n *= -1;
-}
+	if (n < 0)
+		_putchar('-');
+	for (p = 0, i = 1000000000; i > 0; i /= 10)
+	{
+		int d = (n / i) % 10;
 
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
+		d = d < 0 ? -d : d;
+		if (d || p || i == 1)
+		{
+			_putchar(d + '0');
+			p++;
+		}
 
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
-
-}
-
+	}
 }
